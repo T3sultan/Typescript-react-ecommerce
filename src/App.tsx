de9558c05209603dbfced661b0b67ff990c2a5b9
“T3sultan” <incognito.soul011@gmail.com>
2022-08-20 03:13:04 +0600

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import TheLayout from "./views/pages/TheLayout";
 
 function App() {
+  const [state, setState] = useState();
   return (
     <div className="App">
       <TheLayout />
